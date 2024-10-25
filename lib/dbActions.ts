c8cce9c273e9e2d3a95f6ae18626edbf499927d8
OkamotoShu <okamotoshu11@gmail.com>
2024-10-25 20:22:33 +0900

    重複の修正

--- lib/dbActions.ts
+++ lib/dbActions.ts
@@ -95,98 +95,6 @@ export async function fetchLimitedNumberPhotosInfo(num: Number) {
   return photosPathList;
 }
 
-export async function fetchLimitedNumberPhotosInfo(num: Number) {
-  const photosCollection = await adminDB
-    .collection("photos")
-    .orderBy("date", "desc")
-    .limit(num)
-    .get();
-
-  const photosPathList = await Promise.all(
-    photosCollection.docs.map(async (photo: any) => {
-      const id = photo.id;
-      const photoData = photo.data();
-      const userInfoMatchUid = await adminDB
-        .collection("users")
-        .doc(photoData.uid)
-        .get();
-      const nickName = userInfoMatchUid.data().settings.nickName;
-      const currentDate = new Date();
-      const postDate = photoData.date.toDate();
-
-      const setPostDateString = (postDate: Date) => {
-        const diffDate = currentDate.getTime() - postDate.getTime();
-        if (diffDate < 3600000) {
-          return `${Math.floor(diffDate / 60000)}分前`;
-        } else if (diffDate < 86400000) {
-          return `${Math.floor(diffDate / 3600000)}時間前`;
-        } else if (diffDate < 604800000) {
-          return `${Math.floor(diffDate / 86400000)}日前`;
-        }
-        return `${postDate.getFullYear()}年${postDate.getMonth()}月${postDate.getDate()}日`;
-      };
-
-      const postDateString = setPostDateString(postDate);
-
-      return {
-        id: id,
-        nickName: nickName,
-        fav: photoData.fav,
-        url: photoData.url,
-        place: photoData.place,
-        postDate: postDateString,
-      };
-    })
-  );
-  return photosPathList;
-}
-
-export async function fetchLimitedNumberPhotosInfo(num: Number) {
-  const photosCollection = await adminDB
-    .collection("photos")
-    .orderBy("date", "desc")
-    .limit(num)
-    .get();
-
-  const photosPathList = await Promise.all(
-    photosCollection.docs.map(async (photo: any) => {
-      const id = photo.id;
-      const photoData = photo.data();
-      const userInfoMatchUid = await adminDB
-        .collection("users")
-        .doc(photoData.uid)
-        .get();
-      const nickName = userInfoMatchUid.data().settings.nickName;
-      const currentDate = new Date();
-      const postDate = photoData.date.toDate();
-
-      const setPostDateString = (postDate: Date) => {
-        const diffDate = currentDate.getTime() - postDate.getTime();
-        if (diffDate < 3600000) {
-          return `${Math.floor(diffDate / 60000)}分前`;
-        } else if (diffDate < 86400000) {
-          return `${Math.floor(diffDate / 3600000)}時間前`;
-        } else if (diffDate < 604800000) {
-          return `${Math.floor(diffDate / 86400000)}日前`;
-        }
-        return `${postDate.getFullYear()}年${postDate.getMonth()}月${postDate.getDate()}日`;
-      };
-
-      const postDateString = setPostDateString(postDate);
-
-      return {
-        id: id,
-        nickName: nickName,
-        fav: photoData.fav,
-        url: photoData.url,
-        place: photoData.place,
-        postDate: postDateString,
-      };
-    })
-  );
-  return photosPathList;
-}
-
 export async function fetchLikesPhoto() {
   const user = await getUserFromCookie();
   if (!user) return null;
@@ -478,6 +386,16 @@ export async function fetchAllOnlinePrograms() {
 export async function fetchAllPrograms() {
   console.log("Program fetch Executed!")
   const programRef = await adminDB.collection("program").get();
+  const programList: any[] = programRef.docs.map((program: any) => {
+    const programData = program.data();
+    return programData;
+  });
+  return programList;
+}
+
+export async function newFetchAllPrograms() {
+  console.log("Program fetch Executed!")
+  const programRef = await adminDB.collection("test_program2").get();
   const programList: any[] = programRef.docs.map((program: any) => {
     const programData = program.data();
     return programData;
