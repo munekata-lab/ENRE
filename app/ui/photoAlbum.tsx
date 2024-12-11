002b99f3eda03deaef706b0c838b6e01b473aa1b
OkamotoShu <okamotoshu11@gmail.com>
2024-12-11 15:09:13 +0900

    ボタンの色修正とコード内の微調整

--- app/ui/photoAlbum.tsx
+++ app/ui/photoAlbum.tsx
@@ -166,7 +166,7 @@ export default function PhotoAlbumComponent() {
           ))}
           <button
             onClick={handleLoadMore}
-            className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 text-center"
+            className="mt-4 px-6 py-2 bg-green-700 text-white rounded hover:bg-blue-700 text-center"
             >
             さらに表示
           </button>
