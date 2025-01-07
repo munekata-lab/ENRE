2b34f56a30f42d65b6e19c28e2c1d284e6023d91
OkamotoShu <okamotoshu11@gmail.com>
2025-01-08 03:29:10 +0900

    設定後に表示された場合のアナウンス追加

--- app/ui/settingsGuide.tsx
+++ app/ui/settingsGuide.tsx
@@ -13,7 +13,7 @@ export default function SettingsGuideComponent({ settingsMode }: SettingsGuideCo
   const [clicked, setClicked] = useState(false);
   const router = useRouter();
   const title = "初期設定";
-  const content = "ニックネームや帰宅方法、時間割についての登録をしてください";
+  const content = "ニックネームや帰宅方法、時間割についての登録をしてください。";
   //   console.log(`SettingsCompo: ${settingsMode}`);
 
   const handleModalClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -37,17 +37,18 @@ export default function SettingsGuideComponent({ settingsMode }: SettingsGuideCo
             <div className="p-4 bg-white rounded shadow-xl flex flex-col w-11/12 items-center justify-center">
               <h2 className="text-lg font-bold mb-2 text-black">{title}</h2>
               <p className="mb-4 text-black">{content}</p>
+              <p className="mt-1 mb-1 text-xs text-black">※設定後に表示される場合があります。お手数ですが、再読み込みまたはページ移動後に戻ってきてください。</p>
               <div className="flex space-x-4">
                 {!clicked ? (
                   <button
                     onClick={handleModalClick}
                     className="px-4 py-2 bg-white text-green-700 border border-green-700 rounded hover:bg-gray-500"
                   >
-                    設定する
+                    今すぐ設定する
                   </button>
                 ) : (
                   <button className="px-4 py-2 bg-gray-500 text-white border border-green-700 rounded">
-                    設定する
+                    今すぐ設定する
                   </button>
                 )}
               </div>
