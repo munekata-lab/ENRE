54bef2850160a914695afd49841d65dbc77c40a0
OkamotoShu <okamotoshu11@gmail.com>
2025-01-20 10:24:13 +0900

    アンケート公開

--- app/page.tsx
+++ app/page.tsx
@@ -89,6 +89,7 @@ export default async function Home() {
                   {/* 表示速度の改善 */}
                   <WatchCardComponent />
                 </div>
+                {/* bg-gray-400 pointer-events-none ボタンの機能消すやつ */}
                 <div className="row-start-4 col-start-1 col-end-2 text-shadow-lg">
                   <QuestionnaireComponent
                     link={`https://docs.google.com/forms/d/e/1FAIpQLScavjt8Kf2_ZfeY3jfWD6RCd8X848Hp9WMYe_gsn365aFf6ww/viewform?usp=pp_url&entry.296284400=${user.uid}`}
@@ -97,12 +98,12 @@ export default async function Home() {
                     className="text-sm bg-green-700 py-2 px-4 rounded-md font-bold"
                   />
                 </div>
-                <div className="row-start-4 col-start-2 col-end-3 text-shadow-lg pointer-events-none">
+                <div className="row-start-4 col-start-2 col-end-3 text-shadow-lg">
                   <QuestionnaireComponent
                     link={`https://docs.google.com/forms/d/e/1FAIpQLSdcT2TQDyBV5vb5uqELWpT44n4DCoy74Z_lI1b1JWBQcq0l9g/viewform?usp=pp_url&entry.929680888=${user.uid}`}
                     title="体験後アンケート"
-                    detail="1/20(月)以降公開予定"
-                    className="text-sm bg-gray-400 py-2 px-4 rounded-md font-bold"
+                    detail="受付中"
+                    className="text-sm bg-green-700 py-2 px-4 rounded-md font-bold"
                   />
                 </div>
               </div>
